38dfad751c3997841ec655f19cdcbf7de2802399
naqqash12320000 <100236177+naqqash12320000@users.noreply.github.com>
2022-02-26 00:03:47 +0500

    Update ID_List.py

--- ID_List.py
+++ ID_List.py
@@ -1,5 +1,5 @@
-discord_webhook = "https://discord.com/api/webhooks/945778407952764968/Gb3-oy-VVEf0euNPEiqW0nCbP55AmE_wgb5fez04sQB3yvF8unis0oPL4Hv4V3tBBBnl"
-telegram_bot_token = "5131149290:AAHgwS9dGK3L4ssF7Q8dYPsHF8PNSPo7fgg"
+discord_webhook = "https://discord.com/api/ "
+telegram_bot_token = " "
 
 p = {  # names of peoples
     0: "UdemyBot"
